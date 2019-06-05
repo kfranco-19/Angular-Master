a6bc9403c4b1b6adb7b350addeb8693a24b794c0
Franco <kevinfranco199@gmail.com>
2019-06-05 14:21:31 -0600

    new file:   .editorconfig 	new file:   .gitignore 	new file:   README.md 	new file:   angular.json 	new file:   browserslist 	new file:   e2e/protractor.conf.js 	new file:   e2e/src/app.e2e-spec.ts 	new file:   e2e/src/app.po.ts 	new file:   e2e/tsconfig.json 	new file:   karma.conf.js 	new file:   package-lock.json 	new file:   package.json 	new file:   src/app/app-routing.module.ts 	new file:   src/app/app.component.html 	new file:   src/app/app.component.sass 	new file:   src/app/app.component.spec.ts 	new file:   src/app/app.component.ts 	new file:   src/app/app.module.ts 	new file:   src/app/service/notification.service.spec.ts 	new file:   src/app/service/notification.service.ts 	new file:   src/app/service/persona.service.spec.ts 	new file:   src/app/service/persona.service.ts 	new file:   src/assets/.gitkeep 	new file:   src/environments/environment.prod.ts 	new file:   src/environments/environment.ts 	new file:   src/favicon.ico 	new file:   src/index.html 	new file:   src/main.ts 	new file:   src/polyfills.ts 	new file:   src/styles.sass 	new file:   src/test.ts 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.spec.json 	new file:   tslint.json

---
+++ src/app/app.component.ts
@@ -0,0 +1,162 @@
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { PersonaService } from './service/persona.service';
+import { NotificationService } from './service/notification.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'umg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+  title = 'AngularProyect';
+
+  private mensaje: String = null;
+
+  // definir "FormGroup" para ingreso de datos por formulario
+  public formGroup: FormGroup;
+  public mySubject: BehaviorSubject<any>;
+
+  constructor(private personaService: PersonaService,
+              private notificationService: NotificationService) {
+
+        this.mySubject = new BehaviorSubject(null);
+
+  }
+
+  public onClick(): void {
+    console.log('on click');
+  }
+
+  public enviarFormulario(): void {
+    console.log('Datos de formulario:' + JSON.stringify(this.formGroup.value));
+
+    let parametros: any = null;
+    parametros = Object.assign({}, this.formGroup.value);
+
+    let datosAEnviar: any = {
+      nombre: parametros.nombre,
+      apellido: parametros.apellido,
+      apodo: parametros.apodo,
+      correo: parametros.correo,
+      direccion: parametros.direccion
+    };
+
+    console.log('Datos a enviar:' + JSON.stringify(datosAEnviar));
+
+    this.personaService.create(datosAEnviar).subscribe(result => {
+      console.log('Datos from server:' + JSON.stringify(result));
+    });
+  }
+
+  public actualizarFormulario(): void {
+
+    let parametros: any = null;
+    parametros = Object.assign({}, this.formGroup.value);
+
+    let datosAEnviar: any = {
+      nombre: parametros.nombre,
+      apellido: parametros.apellido,
+      apodo: parametros.apodo,
+      correo: parametros.correo,
+      direccion: parametros.direccion
+    };
+
+    console.log('Datos a enviar:' + JSON.stringify(datosAEnviar));
+
+    this.personaService.update(datosAEnviar).subscribe(result => {
+      console.log('Datos from server:' + JSON.stringify(result));
+    });
+
+  }
+
+  public MostrarDatos(): void {
+
+    let parametros: any = null;
+    parametros = Object.assign({}, this.formGroup.value);
+
+    let datosAEnviar: any = {
+      nombre: parametros.nombre,
+      apellido: parametros.apellido,
+      apodo: parametros.apodo,
+      correo: parametros.correo,
+      direccion: parametros.direccion
+    };
+
+    //console.log('Datos a enviar:' + JSON.stringify(datosAEnviar));
+
+    this.personaService.personaList(datosAEnviar).subscribe(result => {
+      console.log('Datos from server:' + JSON.stringify(result));
+    });
+
+  }
+
+  public doNotificationSubscription(): void{
+
+    try{
+      this.notificationService.getPersonaNotification().subscribe((result) =>{
+        this.handleMessageReceived(result);
+      });
+    } catch(e){
+      console.log(e);
+    }
+  }
+
+  private handleMessageReceived(message: any): void {
+    console.log('Menseje recibido' + JSON.stringify(message));
+    this.mySubject.next(message);
+  }
+
+  private initForm(): void {
+    this.formGroup = new FormGroup({
+      nombre: new FormControl('', []
+      ),
+      apellido: new FormControl('', []
+      ),
+      apodo: new FormControl('', []
+      ),
+      correo: new FormControl('', []
+      ),
+      direccion: new FormControl('', []
+      )
+    });
+
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log('on after view');
+  }
+
+  ngOnDestroy(): void {
+    console.log('on destroy');
+  }
+
+  public ProcesarSubject(result: any): void {
+    console.log('hacer algo con: ' + JSON.stringify(result));
+    this.mensaje = this.mensaje + '' + JSON.stringify(result);
+  }
+
+  ngOnInit(): void {
+
+    console.log('on init');
+
+    //this.doNotificationSubscription();
+    // iniciar formulario
+    this.initForm();
+
+    this.mySubject.subscribe(result => {
+      this.ProcesarSubject(result);
+    });
+
+    // ejecutar llamada de servicio restful al iniciar la aplicacion
+    this.personaService
+      .personaList(null)
+      .subscribe((result) => {
+        console.log('RESULTADO:' + JSON.stringify(result));
+      });
+  }
+
+
+}
